8cae2f5bf8d1df17c85a5ac6da0d961434db88e9
yetzt <git@yetzt.me>
2021-08-27 15:00:17 +0200

    add extraction method for svelte based visualizations

--- dataunwrapper.js
+++ dataunwrapper.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 const https = require('https');
+const url = require('url');
 
 const dataunwrapper = module.exports = function(id, fn){
 	if (!id) return fn("usage: dataunwrapper <id>");
@@ -57,6 +58,41 @@ const dataunwrapper = module.exports = function(id, fn){
 
 // extract inline data from over-webpacked code
 const extract = module.exports.extract = function(content, fn){
+
+	// new svelte version, extract __DW_SVELTE_PROPS__ blob
+	if (/window\.__DW_SVELTE_PROPS__/.test(content) && /window\.__DW_SVELTE_PROPS__ = JSON\.parse\("([^\n]+)"\);\n/.test(content)) {
+
+		try {
+			var data = JSON.parse(JSON.parse("\""+RegExp.$1+"\""));
+		} catch (err) {
+			return fn(err);
+		}
+
+		// find csv endpoint in __DW_SVELTE_PROPS__ blob
+		if (!data.hasOwnProperty("assets") || typeof data.assets !== "object") return fn(new Error("data.assets not present"));
+		const endpoint = Object.keys(data.assets).find(function(k){
+			return /\.csv$/.test(k);
+		});
+		if (!endpoint) return fn(new Error("csv url not found in data.assets"));
+		
+		// get csv from url
+		https.get(url.resolve(data.chart.publicUrl, data.assets[endpoint].url), function(res){
+			if (res.statusCode !== 200) fn("response: http status "+res.statusCode);
+
+			let content = [];
+			res.on('error', function(err){
+				fn(err);
+			}).on('data', function(chunk){
+				content.push(chunk);
+			}).on('end', function(){
+				return fn(null, content.join(""))
+			});
+
+		});
+
+		return;
+		
+	}
 
 	// most common
 	if (/\\"(?:data|chartData)/.test(content) && /\\"(?:data|chartData)\\":\\"(.*?[^\\])\\",/.test(content)) {
